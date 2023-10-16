6ef008acf277b4f875f2c600b4b78dd699b56ca8
sachni11 <snimasha99@gmail.com>
2023-10-16 23:07:11 +0530

    edited python.py

---
+++ python.py
@@ -0,0 +1,13 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+
+            return False
+    return True
+
+# Example usage
+number = 13
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
